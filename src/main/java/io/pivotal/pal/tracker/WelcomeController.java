bb5e57fa2b9ff0749bae00d3f7ff09da70c245c9
Rohithps <psrohi@gmail.com>
2020-04-17 19:59:07 +0530

    cloudNativeDeveloperCloudFoundry Third Assignment

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,13 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-
-    @GetMapping("/")
-    public String sayHello() {
-        return "hello";
+    private String message;
+    @GetMapping(path = "/Hello")
+    public String sayHello()  {
+         return "Hello";
     }
+    @GetMapping("/")
+    public String sayHelloFromParam(@Value("${welcome.message}") String message)  {
+        this.message = message;
+        return message;
+    }
+
 }
