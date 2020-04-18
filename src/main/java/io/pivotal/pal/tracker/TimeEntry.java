714d0ff9c493729aa25f4a583b1da7fa58480584
Rohithps <psrohi@gmail.com>
2020-04-18 16:31:47 +0530

    Time Entry

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntry.java
@@ -0,0 +1,21 @@
+package io.pivotal.pal.tracker;
+
+import java.time.LocalDate;
+
+public class TimeEntry {
+    private long id;
+    private long projectId;
+    private long userId;
+    private LocalDate date;
+    private int hours;
+
+    public TimeEntry(long id, long projectId, long userId, LocalDate date, int hours) {
+        this.id = id;
+        this.projectId = projectId;
+        this.userId = userId;
+        this.date = date;
+        this.hours = hours;
+    }
+
+
+}
